07a2d2c04ecf96a1b79f2a0c49721d82aa693b6a
PedroSergioMiguel <pedrosergiomiguel@gmail.com>
2024-10-23 09:22:19 -0300

    Agregar operaciones de multiplicacion y division utilizando metodos

--- Operaciones.java
+++ Operaciones.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Operaciones {
    
-    //MEtodo para sumar dos numeros
+    //MEtodo para sumar
     public static int sumar(int a, int b){
         return a + b;
     }
 
-    //Metodo para restar dos numeros
+    //Metodo para restar
     public static int restar(int a, int b){
         return a - b;
+    }
+
+    // Metodo para multiplicar
+    public static int multiplicar(int a, int b){
+        return a * b;
+    }
+
+    // Metodo para dividir
+    public static double dividir(int a, int b){
+        return a / b;
     }
 
     public static void main(String[] args){
@@ -24,8 +34,12 @@ public class Operaciones {
         // Llamar a los metodos
         int suma = sumar(num1, num2);
         int resta = restar(num1, num2);
+        int multiplicacion  = multiplicar(num1, num2);
+        double division = dividir(num1, num2);
 
         System.out.println("La suma es: " + suma);
         System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicacion es: " + multiplicacion );
+        System.out.println("La division es: " + division);
     }
 }
