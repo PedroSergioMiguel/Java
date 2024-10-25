996f165317371a415b239e37997deadc0f2275ff
PedroSergioMiguel <pedrosergiomiguel@gmail.com>
2024-10-25 10:05:49 -0300

    Agregar interface Clase Superman

--- PruebaInterface.java
+++ PruebaInterface.java
@@ -1,8 +1,10 @@
 public class PruebaInterface {
     public static void main(String[] args){
         Avion miAvion = new Avion();
+        Superman esSuperman = new Superman();
 
         // Llamat al metmodo volar
         miAvion.volar();
+        esSuperman.volar();
     }
 }
---
+++ Superman.java
@@ -0,0 +1,9 @@
+public class Superman implements Volador {
+     
+    // Implementar el metodo Volar
+
+    @Override
+    public void volar(){
+        System.out.println("Superman esta volando.");
+    }
+}
